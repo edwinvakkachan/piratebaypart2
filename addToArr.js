40762c7a55210e52dde8d5f29e48c4c860aab313
edwin vakkachan <edwinvakkachan@gmail.com>
2026-06-16 00:05:30 +0530

    TES

--- addToArr.js
+++ addToArr.js
@@ -274,6 +274,7 @@ const showResult = await pool.query(`
   WHERE tc.trakt_type = 'tv'
     AND tc.imdb_id IS NOT NULL
     AND tc.tvdb_id IS NOT NULL
+    AND tc.imdb_rating IS NOT NULL
     AND COALESCE(tc.trakt_status,'pending') <> 'added'
 
     -- Not in Sonarr
